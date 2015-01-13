609be47bd9098e8a2add0ef63de7688ae19e3802
E. J. Bantz <ejbantz@gmail.com>
2015-01-12 19:36:55 -0600

    Initial Upload

---
+++ .build/Result Resource Layer/js/main.js
@@ -0,0 +1,14 @@
+
+window.onload = function () {
+    // TODO:: Do your initialization job
+
+
+
+    // Sample code
+    var textbox = document.querySelector('.contents');
+    textbox.addEventListener("click", function(){
+    	box = document.querySelector('#textbox');
+    	box.innerHTML = box.innerHTML == "Basic" ? "Sample" : "Basic";
+    });
+    
+};
